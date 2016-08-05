6f295bfe356e96419e701eed3b8281cb1eddba26
SLiX69 <asd2304@gmail.com>
2016-08-06 00:15:04 +0200

    updated endpoints
    endpoints till #16

--- xboxapi/xbox_api.py
+++ xboxapi/xbox_api.py
@@ -10,14 +10,6 @@ class XboxApi:
         """Only requires the XboxApi key"""
         self.api_key = api_key
 
-    def get_user_profile(self, xuid):
-        res = self.request("https://xboxapi.com/v2/{}/profile".format(xuid))
-        return res.json()
-
-    def get_user_gamercard(self, xuid):
-        res = self.request("https://xboxapi.com/v2/{}/gamercard".format(xuid))
-        return res.json()
-        
     def get_profile(self):
         """Return information for current token profile"""
         res = self.request("https://xboxapi.com/v2/profile")
@@ -38,16 +30,73 @@ class XboxApi:
         res = self.request("https://xboxapi.com/v2/conversations")
         return res.json()
 
+    def get_xuid_by_gamertag(self, gamertag):
+        """Return XUID by gamertag"""
+        res = self.request("https://xboxapi.com/v2/xuid/{}".format(gamertag))
+        return res.json()
+
+    def get_gamertag_by_xuid(self, xuid):
+        """Return gamertag by XUID"""
+        res = self.request("https://xboxapi.com/v2/gamertag/{}".format(xuid))
+        return res.json()
+
+    def get_user_profile(self, xuid):
+        """Return profile by XUID"""
+        res = self.request("https://xboxapi.com/v2/{}/profile".format(xuid))
+        return res.json()
+
+    def get_user_gamercard(self, xuid):
+        """Return gamercard by XUID"""
+        res = self.request("https://xboxapi.com/v2/{}/gamercard".format(xuid))
+        return res.json()
+
+    def get_user_presence(self, xuid):
+        """Return current presence information by XUID"""
+        res = self.request("https://xboxapi.com/v2/{}/presence".format(xuid))
+        return res.json()
+
+    def get_user_activity(self, xuid):
+        """Return current activity information by XUID"""
+        res = self.request("https://xboxapi.com/v2/{}/activity".format(xuid))
+        return res.json()
+
+    def get_user_activity_recent(self, xuid):
+        """Return recent activity information by XUID"""
+        res = self.request("https://xboxapi.com/v2/{}/activity/recent".format(xuid))
+        return res.json()
+
+    def get_user_friends(self, xuid):
+        """Return friends by XUID"""
+        res = self.request("https://xboxapi.com/v2/{}/activity/recent".format(xuid))
+        return res.json()
+
+    def get_user_followers(self, xuid):
+        """Return followers by XUID"""
+        res = self.request("https://xboxapi.com/v2/{}/followers".format(xuid))
+        return res.json()
+
+    def get_recent_players(self):
+        """Return recent players by XUID"""
+        res = self.request("https://xboxapi.com/v2/recent-players")
+        return res.json()
+
+    def get_user_gameclips(self, xuid):
+        """Return game clips by XUID"""
+        res = self.request("https://xboxapi.com/v2/{}/game-clips".format(xuid))
+        return res.json()
+
+    # continue with #16
+
     def send_message(self, message, xuids=[]):
         """Send a message to a set of user(s)"""
         headers = {
-                    "X-AUTH" : self.api_key,
-                    "Content-Type" : "application/json"
-                  }
+            "X-AUTH": self.api_key,
+            "Content-Type": "application/json"
+        }
 
         payload = {
-            "message" : message,
-            "to" : []
+            "message": message,
+            "to": []
         }
 
         for xuid in xuids:
@@ -58,6 +107,6 @@ class XboxApi:
 
     def request(self, url):
         """Wrapper on the requests.get"""
-        headers = {"X-AUTH" : self.api_key}
+        headers = {"X-AUTH": self.api_key}
         res = requests.get(url, headers=headers)
         return res
