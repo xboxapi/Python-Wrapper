ba5b00adbfd1e2b9441f3fba76c6458628a61d50
Brandon Davis <dbrandon.91@gmail.com>
2017-06-18 18:09:18 -0700

    PEP8 formatting

--- xboxapi/client.py
+++ xboxapi/client.py
@@ -12,84 +12,91 @@ import xboxapi
 
 logging.basicConfig()
 
+
 class Client(object):
 
-  def __init__(self, api_key=None, timeout=None, lang=None):
+    def __init__(self, api_key=None, timeout=None, lang=None):
 
-    self.api_key =  api_key
-    self.timeout = timeout
-    self.endpoint = 'https://xboxapi.com/v2/'
-    self.timeout = timeout if timeout is not None else 3 # Seconds
-    self.lang = lang
-    self.last_method_call = None
-    self.continuation_token = None
+        self.api_key = api_key
+        self.timeout = timeout
+        self.endpoint = 'https://xboxapi.com/v2/'
+        self.timeout = timeout if timeout is not None else 3  # Seconds
+        self.lang = lang
+        self.last_method_call = None
+        self.continuation_token = None
 
-    # Debug logging can be triggered from environment variable XBOXAPI_DEBUG=1
-    self.logger = logging.getLogger('xboxapi')
-    log_level = logging.DEBUG if os.getenv('XBOXAPI_DEBUG') else logging.INFO
-    self.logger.setLevel(log_level)
+        # Debug logging can be triggered from environment variable
+        # XBOXAPI_DEBUG=1
+        self.logger = logging.getLogger('xboxapi')
+        log_level = logging.DEBUG if os.getenv(
+            'XBOXAPI_DEBUG') else logging.INFO
+        self.logger.setLevel(log_level)
 
-    if self.api_key is None:
-      raise ValueError('Api key is missing')
+        if self.api_key is None:
+            raise ValueError('Api key is missing')
 
-  def gamer(self, gamertag=None, xuid=None):
-    ''' return a gamer object '''
-    if gamertag is None:
-      raise ValueError("No gamertag given!")
+    def gamer(self, gamertag=None, xuid=None):
+        ''' return a gamer object '''
+        if gamertag is None:
+            raise ValueError('No gamertag given!')
 
-    return Gamer(gamertag=gamertag, client=self, xuid=xuid)
+        return Gamer(gamertag=gamertag, client=self, xuid=xuid)
 
-  def api_get(self, method):
-    ''' GET wrapper on requests library '''
-    headers = {'X-Auth' : self.api_key,
-               'User-Agent' : 'Python/XboxApi ' + xboxapi.__version__}
+    def api_get(self, method):
+        ''' GET wrapper on requests library '''
+        headers = {'X-Auth': self.api_key,
+                   'User-Agent': 'Python/XboxApi ' + xboxapi.__version__}
 
-    if self.lang is not None:
-      headers['Accept-Language'] = self.lang
+        if self.lang is not None:
+            headers['Accept-Language'] = self.lang
 
-    url = self.endpoint + method
-    # Check for continuation token and the method match the last call
-    if method == self.last_method_call and self.continuation_token is not None:
-      url = url + '?continuationToken=' + self.continuation_token
+        url = self.endpoint + method
+        # Check for continuation token and the method match the last call
+        if method == self.last_method_call and self.continuation_token is not None:
+            url = url + '?continuationToken=' + self.continuation_token
 
-    self.logger.debug('{} {}'.format('GET', url))
-    self.logger.debug('Headers: {}'.format(headers))
+        self.logger.debug('{} {}'.format('GET', url))
+        self.logger.debug('Headers: {}'.format(headers))
 
-    res = requests.get(self.endpoint + method, headers=headers, timeout=self.timeout)
-    self.logger.debug('Response: {}'.format(res.json()))
+        res = requests.get(self.endpoint + method,
+                           headers=headers, timeout=self.timeout)
+        self.logger.debug('Response: {}'.format(res.json()))
 
-    # Track method calls and peak for continuation token
-    self.last_method_call = method
-    self.continuation_token = None
-    if 'X-Continuation-Token' in res.headers:
-      self.continuation_token = res.headers['X-Continuation-Token']
+        # Track method calls and peak for continuation token
+        self.last_method_call = method
+        self.continuation_token = None
+        if 'X-Continuation-Token' in res.headers:
+            self.continuation_token = res.headers['X-Continuation-Token']
 
-    return res
+        return res
 
-  def api_post(self, method, body):
-    ''' POST wrapper on requests library '''
-    headers = {
-        'X-AUTH': self.api_key,
-        'Content-Type' : 'application/json'
-    }
+    def api_post(self, method, body):
+        ''' POST wrapper on requests library '''
+        headers = {
+            'X-AUTH': self.api_key,
+            'Content-Type': 'application/json'
+        }
 
-    url = '{}{}'.format(self.endpoint, method)
+        url = '{}{}'.format(self.endpoint, method)
 
-    self.logger.debug('{} {}'.format('POST', url))
-    self.logger.debug('Headers: {}'.format(headers))
-    self.logger.debug('Body: {}'.format(body))
+        self.logger.debug('{} {}'.format('POST', url))
+        self.logger.debug('Headers: {}'.format(headers))
+        self.logger.debug('Body: {}'.format(body))
 
-    res = requests.post(self.endpoint + method, headers=headers, data=json.dumps(body),
-                         timeout=self.timeout)
-    self.logger.debug('Response: {}'.format(res.json()))
+        res = requests.post(self.endpoint + method, headers=headers, data=json.dumps(body),
+                            timeout=self.timeout)
+        self.logger.debug('Response: {}'.format(res.json()))
 
-    return res
+        return res
 
-  def calls_remaining(self):
-    ''' Check on the limits from server '''
-    server_headers = self.api_get('accountxuid').headers
-    limit_headers = {}
-    limit_headers['X-RateLimit-Reset'] = server_headers['X-RateLimit-Reset']
-    limit_headers['X-RateLimit-Limit'] = server_headers['X-RateLimit-Limit']
-    limit_headers['X-RateLimit-Remaining'] = server_headers['X-RateLimit-Remaining']
-    return limit_headers
+    def calls_remaining(self):
+        ''' Check on the limits from server '''
+        server_headers = self.api_get('accountxuid').headers
+        limit_headers = {}
+        limit_headers[
+            'X-RateLimit-Reset'] = server_headers['X-RateLimit-Reset']
+        limit_headers[
+            'X-RateLimit-Limit'] = server_headers['X-RateLimit-Limit']
+        limit_headers[
+            'X-RateLimit-Remaining'] = server_headers['X-RateLimit-Remaining']
+        return limit_headers
--- xboxapi/gamer.py
+++ xboxapi/gamer.py
@@ -1,92 +1,93 @@
 #-*- coding: utf-8 -*-
 
+
 class Gamer(object):
-  ''' Xbox profile wrapper '''
+    ''' Xbox profile wrapper '''
 
-  def __init__(self, gamertag=None, client=None, xuid=None):
-    self.client = client
-    self.gamertag = gamertag
-    self.xuid = xuid if xuid is not None else self.fetch_xuid()
-    self.endpoints = ['profile',
-                      'messages',
-                      'conversations',
-                      'recent-players',
-                      'activity-feed',
-                      'latest-xbox360-games',
-                      'latest-xboxone-games',
-                      'latest-xboxone-apps',
-                      'xboxone-gold-lounge']
-    self.endpoints_xuid = ['achievements',
-                           'presence',
-                           'gamercard',
-                           'activity',
-                           'friends',
-                           'followers',
-                           'game-clips',
-                           'game-clips/saved',
-                           'game-stats',
-                           'screenshots',
-                           'xbox360games',
-                           'xboxonegames',
-                           'game-status']
+    def __init__(self, gamertag=None, client=None, xuid=None):
+        self.client = client
+        self.gamertag = gamertag
+        self.xuid = xuid if xuid is not None else self.fetch_xuid()
+        self.endpoints = ['profile',
+                          'messages',
+                          'conversations',
+                          'recent-players',
+                          'activity-feed',
+                          'latest-xbox360-games',
+                          'latest-xboxone-games',
+                          'latest-xboxone-apps',
+                          'xboxone-gold-lounge']
+        self.endpoints_xuid = ['achievements',
+                               'presence',
+                               'gamercard',
+                               'activity',
+                               'friends',
+                               'followers',
+                               'game-clips',
+                               'game-clips/saved',
+                               'game-stats',
+                               'screenshots',
+                               'xbox360games',
+                               'xboxonegames',
+                               'game-status']
 
-  def get(self, method=None, term=None):
-    ''' Retrieve data from supported endpoints '''
-    url = self.parse_endpoints(method, term)
-    if url is not False:
-      return self.client.api_get(url).json()
+    def get(self, method=None, term=None):
+        ''' Retrieve data from supported endpoints '''
+        url = self.parse_endpoints(method, term)
+        if url is not False:
+            return self.client.api_get(url).json()
 
-    url = self.parse_endpoints_secondary(method, term)
-    if url is not False:
-      return self.client.api_get(url).json()
+        url = self.parse_endpoints_secondary(method, term)
+        if url is not False:
+            return self.client.api_get(url).json()
 
-    return {}
+        return {}
 
-  def parse_endpoints(self, method=None, term=None):
-    ''' Constructs a valid endpoint url for api '''
-    if method is None:
-      return False
+    def parse_endpoints(self, method=None, term=None):
+        ''' Constructs a valid endpoint url for api '''
+        if method is None:
+            return False
 
-    for endpoint in self.endpoints:
-      if endpoint != method:
-        continue
-      url = endpoint
-      if term is not None:
-        url = url + '/' + term
-      return url
+        for endpoint in self.endpoints:
+            if endpoint != method:
+                continue
+            url = endpoint
+            if term is not None:
+                url = url + '/' + term
+            return url
 
-    return False
+        return False
 
-  def parse_endpoints_secondary(self, method=None, term=None):
-    ''' Parse secondary endpoints that require xuid in url '''
-    for endpoint in self.endpoints_xuid:
-      if endpoint != method:
-        continue
-      url = str(self.xuid) + '/' + endpoint
-      if term is not None:
-        url = url + '/' + term
-      return url
+    def parse_endpoints_secondary(self, method=None, term=None):
+        ''' Parse secondary endpoints that require xuid in url '''
+        for endpoint in self.endpoints_xuid:
+            if endpoint != method:
+                continue
+            url = str(self.xuid) + '/' + endpoint
+            if term is not None:
+                url = url + '/' + term
+            return url
 
-    return False
+        return False
 
-  def send_message(self, message=None, xuids=None):
-    ''' Send a message given a list of gamer xuids '''
-    payload = {}
-    if message is None:
-      raise ValueError('A message is required!')
-    if xuids is None or not hasattr(xuids, 'append'):
-      raise TypeError('List was not given!')
-    payload['to'] = xuids
-    payload['message'] = message
-    return self.client.api_post('messages', payload)
+    def send_message(self, message=None, xuids=None):
+        ''' Send a message given a list of gamer xuids '''
+        payload = {}
+        if message is None:
+            raise ValueError('A message is required!')
+        if xuids is None or not hasattr(xuids, 'append'):
+            raise TypeError('List was not given!')
+        payload['to'] = xuids
+        payload['message'] = message
+        return self.client.api_post('messages', payload)
 
-  def post_activity(self, message=None):
-    ''' Post directly to your activity feed '''
-    if message is None:
-      raise ValueError('A message is required!')
-    payload['message'] = message
-    return self.client.api_post('acitivity-feed', payload)
+    def post_activity(self, message=None):
+        ''' Post directly to your activity feed '''
+        if message is None:
+            raise ValueError('A message is required!')
+        payload['message'] = message
+        return self.client.api_post('acitivity-feed', payload)
 
-  def fetch_xuid(self):
-    ''' Fetch gamer xuid from gamertag '''
-    return self.client.api_get('xuid/' + self.gamertag).json()
+    def fetch_xuid(self):
+        ''' Fetch gamer xuid from gamertag '''
+        return self.client.api_get('xuid/' + self.gamertag).json()
